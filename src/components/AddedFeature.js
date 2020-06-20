b8b40c6d9420d73644a3553c131abe4432e89888
Ryan Schueck <ryschueck@gmail.com>
2020-06-19 21:00:04 -0400

    met MVP

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const AddedFeature = props => {
+const AddedFeature = ({ removeFeatures, feature }) => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
-      {props.feature.name}
+
+      <button
+        onClick={() => { removeFeatures(feature) }}
+        className="button">X</button>
+      {feature.name}
     </li>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {featureReducers} from './reducers/featureReducers';
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(featureReducers);
+
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>,
+ rootElement);
